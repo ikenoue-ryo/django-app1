07f79c99a4390c08a8b041b25b33f4e472c76982
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-10-13 22:41:52 +0900

    投稿フォームの作成

--- frontend/src/pages/PostPage.vue
+++ frontend/src/pages/PostPage.vue
@@ -1,104 +1,104 @@
 <template>
-  <v-app>
-    <div id="home-page">
-      <GlobalHeader/>
-      <GlobalMessage/>
+  <div id="app" class="back_body">
+    <GlobalHeader />
 
-      <!-- メインエリア -->
-      <main class="container">
-        <p class="h5 mb-4">ホーム</p>
-        <b-form @submit.prevent="submitSave">
-          <div class="row form-group">
-            <label class="col-sm-3 col-form-label">タイトル</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" v-model="form.post.title">
-            </div>
-          </div>
-          <div class="row form-group">
-            <label class="col-sm-3 col-form-label">本文</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" v-model="form.post.text">
-            </div>
-          </div>
-          <div class="row form-group">
-            <label class="col-sm-3 col-form-label">価格</label>
-            <div class="col-sm-8">
-              <input type="text" class="form-control" v-model="form.post.price">
-            </div>
-          </div>
-          <div class="row text-center mt-5">
-            <div class="col-sm-12">
-              <b-button type="submit" variant="primary">{{ isCreated ? '更新' : '登録' }}</b-button>
-            </div>
-          </div>
-        </b-form>
-      </main>
-    </div>
-  </v-app>
+    <h2>Let's Post</h2>
+    <v-card
+      class="card_style"
+      max-width="800"
+    >
+      <quillEditor/>
+    </v-card>
+    <PrFooter/>
+  </div>
 </template>
 
 <script>
-  import api from '@/services/api'
-  import GlobalHeader from '@/components/GlobalHeader.vue'
-  import GlobalMessage from '@/components/GlobalMessage.vue'
-  export default {
-    components: {
-      GlobalHeader,
-      GlobalMessage
-    },
-    data () {
-      return {
-        form: {
-          post: {
-            title: '',
-            text: '',
-            price: 0,
-          }
-        }
-      }
-    },
-    computed: {
-      isCreated: function () {
-        console.log('これ', this.form.post.id)
-        return this.form.post.id !== undefined
-      }
-    },
-    methods: {
-      // 登録・更新ボタン押下
-      submitSave: function () {
-        api({
-          // 登録済みかどうかでHTTPメソッドとエンドポイントを切り替える
-          method: this.isCreated ? 'put' : 'post',
-          url: this.isCreated ? '/posts/' + this.form.post.id + '/' : '/posts/',
-          data: {
-            'id': this.form.post.id,
-            'title': this.form.post.title,
-            'text': this.form.post.text,
-            'price': this.form.post.price
-          }
-        })
-          .then(response => {
-            const message = this.isCreated ? '更新しました。' : '登録しました。'
-            this.$store.dispatch('message/setInfoMessage', { message: message })
-            this.form.post = response.data
-          })
+import GlobalHeader from '../components/GlobalHeader'
+import PrFooter from '../components/PrFooter'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
+export default{
+  components:{
+    GlobalHeader,
+    PrFooter,
+    quillEditor
+  },
+};
+</script>
+
+
+<style>
+/* エディタ専用のCSS(SCSSが効かない) */
+.ql-editor p{
+  font-size: 20px;
+}
+
+.ql-editor ::placeholder{
+  color: orange;
+}
+
+.ql-container{
+  border: none!important;
+}
+</style>
+
+
+
+<style lang="scss" scoped>
+p{
+  font-size: 1.2rem;
+  font-family: Rubik, -apple-system, "Hiragino Sans", "Hiragino Kaku Gothic ProN", BlinkMacSystemFont, YuGothic, "Yu Gothic", sans-serif;
+}
+
+.back_body{
+  background-color: #eee;
+  overflow: hidden;
+  
+
+  h2{
+    text-align: center;
+    font-size: 45px;
+    font-weight: 600;
+    text-align: center;
+    margin: 30px;
+  }
+
+  .card_style{
+    margin: 30px auto;
+    padding: 50px;
+    border-top: 5px solid #33b5e5;
+    min-height: 100vh;
+
+    .back-color{
+      a.start{
+        background-color: #329eff!important;
+        font-size: 0.9rem;
+        color: #fff;
+        font-weight: bold;
+        margin-top: 20px;
+        padding: 20px;
       }
     }
   }
-</script>
 
-<style lang="scss" scoped>
-#app{
-    font-family: Rubik, "Noto Sans JP", sans-serif; 
-    // Arial, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, メイリオ, Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
-    background-color: #eee;
+  .image_area{
+    margin-bottom: 20px;
+
+    img{
+      width: 700px;
+      height: 400px;
+      object-fit: contain;
+    }
+  }
 }
 
-.container {
-  max-width: 900px;
-}
 
-.fullheight {
-    height: 100%;
+.v-input{
+  font-size: 3rem;
 }
 </style>
