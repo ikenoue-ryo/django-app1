193e4e466a6ce34c3d5595ae41970f9a2f008997
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-11-08 15:21:55 +0900

    Tagテストの追加

---
+++ config/apiv1/tests/test_views.py
@@ -0,0 +1,90 @@
+from django.contrib.auth import get_user_model
+from django.utils.timezone import localtime
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import Tag
+
+class TestTagCreateAPIView(APITestCase):
+
+    TARGET_URL = '/api/v1/tag/'
+
+    def test_create_success(self):
+        """タグモデル"""
+
+        params = {
+            'name': 'tag_test'
+        }
+        response = self.client.post(self.TARGET_URL, params, format='json')
+
+        # DBの状態を検証
+        self.assertEqual(Tag.objects.count(), 1)
+        # レスポンスのステータス検証
+        self.assertEqual(response.status_code, 201)
+        tag = Tag.objects.get()
+        expected_json_dict = {
+            'id': tag.id,
+            'name': tag.name,
+        }
+        # print('response.content', response.content)
+        self.assertJSONEqual(response.content, expected_json_dict)
+
+    
+    def test_create_bad_request(self):
+        """タグモデル"""
+
+        # APIリクエストを実行
+        params = {
+            'name': '',
+        }
+        response = self.client.post(self.TARGET_URL, params, format='json')
+
+        # DBの状態を検証
+        self.assertEqual(Tag.objects.count(), 0)
+        # レスポンスのステータス検証
+        self.assertEqual(response.status_code, 400)
+        
+
+
+class TestTagUpdateAPIView(APITestCase):
+
+    TARGET_URL_WITH_PK = '/api/v1/tag/{}/'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = get_user_model().objects.create_user(
+            username='user',
+            email='user@example.com',
+            password='secret',
+        )
+
+    def test_update_success(self):
+
+        # Cookie認証ログイン
+        # self.client.login(username='user', password='secret')
+
+        # JWTログイン
+        token = str(RefreshToken.for_user(self.user).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+
+        # APIリクエストを実行
+        tag = Tag.objects.create(
+            name='tag_name'
+        )
+        params = {
+            'id': tag.id,
+            'name': tag.name,
+        }
+        response = self.client.put(
+            self.TARGET_URL_WITH_PK.format(tag.id), params, format='json',
+        )
+        
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+        tag = Tag.objects.get()
+        expected_json_dict = {
+            'id': tag.id,
+            'name': tag.name,
+        }
+        self.assertJSONEqual(response.content, expected_json_dict)
