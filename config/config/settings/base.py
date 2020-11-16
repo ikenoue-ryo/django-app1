5453db53e8b4aae088ee21e5e613d4167d8e291e
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-11-16 14:35:55 +0900

    CIにCSSデプロイ追加

--- config/config/settings/base.py
+++ config/config/settings/base.py
@@ -216,7 +216,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS-Settings
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'vue-django'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_LOCATION = 'static' # s3バケット上のベースとなるファイルパス
 
 # STATIC-Files
