bd1d00ac6cc981d4e0f7ee64458bb985f3b7348b
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-11-26 23:56:11 +0900

    circleci修正

--- config/config/urls.py
+++ config/config/urls.py
@@ -5,7 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView, RedirectView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
